aa32f2939e243ae9a19fe1733bcdad306ac8a7a4
ultrabear <bearodark@gmail.com>
2020-12-13 23:25:09 -0800

    remove legacy code

--- cmds/lib_loaders.py
+++ cmds/lib_loaders.py
@@ -77,41 +77,6 @@ def load_blacklist(guild_id):
         return blacklist
 
 
-# Old blacklist loader
-def load_blacklist_old(guild_id):
-    try:
-        with open(f"datastore/{guild_id}.cache.db", "r") as blacklist_cache:
-            return json.load(blacklist_cache)
-    except FileNotFoundError:
-        db = db_handler()
-        blacklist = {}
-
-        # Loads base db
-        for i in ["word-blacklist","regex-blacklist","filetype-blacklist"]:
-            try:
-                blacklist[i] = db.fetch_rows_from_table(f"{guild_id}_config", ["property",i])[0][1]
-            except db_error.OperationalError:
-                blacklist[i] = []
-            except IndexError:
-                blacklist[i] = []
-        db.close()
-
-        # Loads regex
-        if blacklist["regex-blacklist"]:
-            blacklist["regex-blacklist"] = [i.split(" ")[1][1:-2] for i in json.loads(blacklist["regex-blacklist"])["blacklist"]]
-        else:
-            blacklist["regex-blacklist"] = []
-
-        # Loads word, filetype blacklist
-        for i in ["word-blacklist","filetype-blacklist"]:
-            if blacklist[i]:
-                blacklist[i] = blacklist[i].lower().split(",")
-
-        with open(f"datastore/{guild_id}.cache.db", "w") as blacklist_cache:
-            json.dump(blacklist, blacklist_cache)
-        return blacklist
-
-
 def generate_infractionid():
     try:
         num_words = os.path.getsize("datastore/wordlist.cache.db")-1
@@ -139,13 +104,3 @@ def generate_infractionid():
             for i in structured_data:
                 structured_data_file.write(i+bytes(maxval-len(i)))
         return generate_infractionid()
-
-
-def generate_infractionid_old():
-    with open("common/wordlist.txt", "r") as words:
-        wordlist = words.read().split("\n")
-        output = ""
-        for i in range(3):
-            preout = random.choice(wordlist)
-            output += preout[0].upper()+preout[1:]
-    return output
