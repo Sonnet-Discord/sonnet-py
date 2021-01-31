34ca6c4866cbbb39fb27b914d806f862ab47dede
atrt7 <turebyluro@gmail.com>
2021-01-30 18:47:18 -0600

    Add h joke command

--- cmds/cmd_jokes.py
+++ cmds/cmd_jokes.py
@@ -1,5 +1,5 @@
 # Joke commands idk mck wanted this
-# Ultrabear 2020
+# Ultrabear & atrt7 2021
 
 
 async def joke_ban_user(message, args, client, **kwargs):
@@ -20,9 +20,27 @@ async def joke_ban_user(message, args, client, **kwargs):
 
     await message.channel.send(f"'Banned' {user.mention} with ID {user.id} for {reason}")
 
+async def h_command(message, args, client, **kwargs):
+
+    await message.channel.send("h lmao haha funny I said h now laugh")
 
 category_info = {'name': 'jokes', 'pretty_name': 'Jokes', 'description': 'Joke commands, because because'}
 
-commands = {'jban': {'pretty_name': 'jban <uid>', 'description': '"bans" a user', 'permission': 'moderator', 'cache': 'keep', 'execute': joke_ban_user}}
+commands = {
+    'jban': {
+        'pretty_name': 'jban <uid>',
+        'description': '"bans" a user',
+        'permission': 'moderator',
+        'cache': 'keep',
+        'execute': joke_ban_user
+        },
+    'h': {
+        'pretty_name': 'h',
+        'description': 'h',
+        'permission': 'everyone',
+        'cache': 'keep',
+        'execute': h_command
+    }
+}
 
-version_info = "1.1.2"
+version_info = "1.1.4-DEV"
