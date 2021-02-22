1d8daa164e268efefd9612705a1bef86ff37390d
Saragon4005 <domikakeszei@gmail.com>
2021-02-22 14:50:54 -0800

    add jban deprecation help

--- cmds/cmd_jokes.py
+++ cmds/cmd_jokes.py
@@ -23,8 +23,27 @@ async def joke_ban_user(message, args, client, **kwargs):
         await message.channel.send(f"'Banned' {user.mention} with ID {user.id} for {reason}")
 
 
+async def joke_ban_deprecation(message, args, client, **kwargs):
+    await message.send("`jban` is deprecated, please us `jkb` instead")
+
+
 category_info = {'name': 'jokes', 'pretty_name': 'Jokes', 'description': 'Joke commands, because because'}
 
-commands = {'jkb': {'pretty_name': 'jkb <uid>', 'description': '"bans" a user', 'permission': 'moderator', 'cache': 'keep', 'execute': joke_ban_user}}
+commands = {
+    'jkb': {
+        'pretty_name': 'jkb <uid>',
+        'description': '"bans" a user',
+        'permission': 'moderator',
+        'cache': 'keep',
+        'execute': joke_ban_user
+        },
+    'jban': {
+        'pretty_name': 'jban',
+        'description': 'deprecated use jkb instead',
+        'permission': 'moderator',
+        'cache': 'keep',
+        'execute': joke_ban_deprecation
+        }
+    }
 
 version_info = "1.1.4"
