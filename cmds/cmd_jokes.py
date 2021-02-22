e950dc248150117c3165a0e2377ade53c989fe75
ultrabear <bearodark@gmail.com>
2021-02-22 10:38:07 -0800

    revamp jban command (thanks @Saragon4005)

--- cmds/cmd_jokes.py
+++ cmds/cmd_jokes.py
@@ -12,21 +12,19 @@ async def joke_ban_user(message, args, client, **kwargs):
     try:
         user = client.get_user(int(args[0].strip("<@!>")))
     except ValueError:
-        await message.channel.send("Invalid User")
-        return
+        user = None
     except IndexError:
         await message.channel.send("No user specified")
         return
 
     if not user:
-        await message.channel.send("Invalid User")
-        return
-
-    await message.channel.send(f"'Banned' {user.mention} with ID {user.id} for {reason}")
+        await message.channel.send(f"'Banned' {user} for {reason}")
+    else:
+        await message.channel.send(f"'Banned' {user.mention} with ID {user.id} for {reason}")
 
 
 category_info = {'name': 'jokes', 'pretty_name': 'Jokes', 'description': 'Joke commands, because because'}
 
-commands = {'jb': {'pretty_name': 'jb <uid>', 'description': '"bans" a user', 'permission': 'moderator', 'cache': 'keep', 'execute': joke_ban_user}}
+commands = {'jkb': {'pretty_name': 'jkb <uid>', 'description': '"bans" a user', 'permission': 'moderator', 'cache': 'keep', 'execute': joke_ban_user}}
 
 version_info = "1.1.4-DEV"
