603ff6b9e7903e7b055fcff35e0b5661ecb6114b
ultrabear <bearodark@gmail.com>
2021-03-29 23:42:33 -0700

    much better (retab C loader, no code change)

--- libs/sfdbcloader.c
+++ libs/sfdbcloader.c
@@ -4,52 +4,52 @@
 
 int load_words ( char* filename, int retamount, unsigned long long seed, char* pointer, int pointer_length ) {
 
-    // Open cache file
-    FILE* fp = fopen(filename, "rb");
+	// Open cache file
+	FILE* fp = fopen(filename, "rb");
 
-    // Exit if file does not exist
-    if ( fp == NULL ) {
-        return 1;
-    }
+	// Exit if file does not exist
+	if ( fp == NULL ) {
+		return 1;
+	}
 
-    // Grab length of file
-    int maxln = fgetc(fp);
-    fseek(fp, 0, SEEK_END);
-    int size = (ftell(fp) - 1) / maxln ;
+	// Grab length of file
+	int maxln = fgetc(fp);
+	fseek(fp, 0, SEEK_END);
+	int size = (ftell(fp) - 1) / maxln ;
 
-    // Make mini buffer to hold words
-    char buf[256];
+	// Make mini buffer to hold words
+	char buf[256];
 
-    // Seed random number generator
-    srand(seed);
+	// Seed random number generator
+	srand(seed);
 
-    // Empty buffer
-    memset(pointer, 0, pointer_length);
+	// Empty buffer
+	memset(pointer, 0, pointer_length);
 
-    // Grab X amount of words
-    for ( int i = 0 ; i < retamount; i++ ) {
+	// Grab X amount of words
+	for ( int i = 0 ; i < retamount; i++ ) {
 
-        // Seek to random word pointer
-        fseek(fp, (((rand() % (size)) * maxln) + 1), SEEK_SET);
+		// Seek to random word pointer
+		fseek(fp, (((rand() % (size)) * maxln) + 1), SEEK_SET);
 
-        // Grab word and add it to buffer
-        // The plus one makes no sense i think it just reads one less than its supposed to cause \x00 terminator?
-        fgets(buf, fgetc(fp)+1, fp);
+		// Grab word and add it to buffer
+		// The plus one makes no sense i think it just reads one less than its supposed to cause \x00 terminator?
+		fgets(buf, fgetc(fp)+1, fp);
 
-        // Only add to buffer if it has space
-        if ( strlen(buf) + strlen(pointer) < pointer_length ) {
-            strcat(pointer, buf);
-        }
-        else {
-            fclose(fp);
-            return 1;
-        }
+		// Only add to buffer if it has space
+		if ( strlen(buf) + strlen(pointer) < pointer_length ) {
+			strcat(pointer, buf);
+		}
+		else {
+			fclose(fp);
+			return 1;
+		}
 
-    }
+	}
 
-    // Close file pointer
-    fclose(fp);
-    return 0;
+	// Close file pointer
+	fclose(fp);
+	return 0;
 }
 
 int load_words_test( char* filename, int retamount, unsigned long long seed, char* pointer, int pointer_length, int testcount) {
