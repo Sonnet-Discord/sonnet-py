a4201d5f849f4c9982e822b1a35067c56003513d
ultrabear <bearodark@gmail.com>
2021-04-02 05:42:14 -0700

    thats a little more elegant

--- libs/lib_db_obfuscator.py
+++ libs/lib_db_obfuscator.py
@@ -8,5 +8,7 @@ import lib_sonnetdb
 importlib.reload(lib_sonnetdb)
 from lib_sonnetdb import db_hlapi
 
-# Test connection on import and make pyflakes shut up
-db_hlapi(None).close()
+
+# make pyflakes shut up
+def getdb():
+    return db_hlapi
