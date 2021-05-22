a557d9665d8e574e06cb48c9d6aa255b7cffe43a
ultrabear <bearodark@gmail.com>
2021-05-22 11:41:05 -0700

    this is as close as we get to constants, at least its there

--- libs/lib_constants.py
+++ libs/lib_constants.py
@@ -1,40 +1,41 @@
 # Constants cause discord likes to troll us
 # Ultrabear 2021
 
+from typing import Final
 
 class message:
-    content: int = 2000
+    content: Final[int] = 2000
 
 
 class embed:
 
-    title: int = 256
-    description: int = 2048
-    author: int = 256
+    title: Final[int] = 256
+    description: Final[int] = 2048
+    author: Final[int] = 256
 
     class field:
-        name: int = 256
-        value: int = 1024
+        name: Final[int] = 256
+        value: Final[int] = 1024
 
-    footer: int = 2048
+    footer: Final[int] = 2048
 
 
 class sonnet:
 
-    error_embed: str = "ERROR: The bot does not have permissions to send embeds here"
+    error_embed: Final[str] = "ERROR: The bot does not have permissions to send embeds here"
 
     class error_args:
-        not_enough: str = "ERROR: Not enough args supplied"
+        not_enough: Final[str] = "ERROR: Not enough args supplied"
 
     class error_channel:
-        none: str = "ERROR: No channel supplied"
-        invalid: str = "ERROR: Channel is not a valid channel"
-        scope: str = "ERROR: Channel is not in guild"
+        none: Final[str] = "ERROR: No channel supplied"
+        invalid: Final[str] = "ERROR: Channel is not a valid channel"
+        scope: Final[str] = "ERROR: Channel is not in guild"
 
     class error_role:
-        none: str = "ERROR: No role supplied"
-        invalid: str = "ERROR: Role is not valid role"
+        none: Final[str] = "ERROR: No role supplied"
+        invalid: Final[str] = "ERROR: Role is not valid role"
 
     class error_message:
-        invalid: str = "ERROR: Message does not exist"
+        invalid: Final[str] = "ERROR: Message does not exist"
 
