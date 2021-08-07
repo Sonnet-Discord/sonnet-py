649e7c2087f5727b3bc05e7d8f0c398cc91c91f1
ultrabear <bearodark@gmail.com>
2021-08-07 15:45:06 -0700

    this is my final response to message caching bullshit (add starboard-forceboard)

--- cmds/cmd_starboard.py
+++ cmds/cmd_starboard.py
@@ -11,13 +11,17 @@ importlib.reload(lib_db_obfuscator)
 import lib_parsers
 
 importlib.reload(lib_parsers)
+import lib_constants
+
+importlib.reload(lib_constants)
 import lib_loaders
 
 importlib.reload(lib_loaders)
 
-from lib_parsers import parse_boolean, update_log_channel
+from lib_parsers import parse_boolean, update_log_channel, parse_channel_message, generate_reply_field
 from lib_loaders import load_message_config
 from lib_db_obfuscator import db_hlapi
+import lib_constants as constants
 
 from sonnet_cfg import STARBOARD_EMOJI, STARBOARD_COUNT
 
@@ -94,6 +98,52 @@ async def set_starboard_count(message: discord.Message, args: List[str], client:
         await message.channel.send(f"Starboard count is {count}")
 
 
+async def force_starboard(message: discord.Message, args: List[str], client: discord.Client, **kwargs: Any) -> Any:
+    if not message.guild:
+        return 1
+
+    mconf = load_message_config(message.guild.id, kwargs["ramfs"], datatypes=starboard_types)
+
+    try:
+        starmessage, _ = await parse_channel_message(message, args, client)
+    except lib_parsers.errors.message_parse_failure:
+        return 1
+
+    if (channel_id := mconf["starboard-channel"]) and (channel := client.get_channel(int(channel_id))):
+
+        with db_hlapi(message.guild.id) as db:
+            db.inject_enum("starboard", [
+                ("messageID", str),
+                ])
+
+            # Add to starboard
+            db.set_enum("starboard", [str(starmessage.id)])
+
+        # Generate replies
+        message_content = generate_reply_field(starmessage)
+
+        # Generate embed
+        starboard_embed = discord.Embed(title="Starred message", description=message_content, color=0xffa700)
+
+        for i in starmessage.attachments:
+            if any([i.url.endswith(ext) for ext in [".png", ".bmp", ".jpg", ".jpeg", ".gif", ".webp"]]):
+                starboard_embed.set_image(url=i.url)
+
+        starboard_embed.set_author(name=starmessage.author, icon_url=starmessage.author.avatar_url)
+        starboard_embed.timestamp = starmessage.created_at
+        starboard_embed.set_footer(text=f"#{starmessage.channel}")
+
+        try:
+            await channel.send(embed=starboard_embed)
+        except discord.errors.Forbidden:
+            await message.channel.send(constants.sonnet.error_embed)
+            return 1
+
+    else:
+        await message.channel.send("ERROR: No starboard channel")
+        return 1
+
+
 category_info: Dict[str, str] = {'name': 'starboard', 'pretty_name': 'Starboard', 'description': 'Starboard commands.'}
 
 commands: Dict[str, Dict[str, Any]] = {
@@ -128,7 +178,15 @@ commands: Dict[str, Dict[str, Any]] = {
             'permission': 'administrator',
             'cache': 'direct:(f)caches/sonnet_starboard',
             'execute': set_starboard_count
-            }
+            },
+    'starboard-forceboard':
+        {
+            'pretty_name': 'starboard-forceboard <message>',
+            'description': 'Forcibly starboard a message and add it to the starboard db',
+            'permission': 'administrator',
+            'cache': 'keep',
+            'execute': force_starboard
+            },
     }
 
 version_info: str = "pre2.0.0-DEV"
