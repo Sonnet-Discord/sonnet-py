0d34de177dba1a934e22e84f9b70cc5f14a2b052
ultrabear <bearodark@gmail.com>
2022-02-03 23:06:47 -0800

    specify py version in pylint test

--- build_tools/autotest.py
+++ build_tools/autotest.py
@@ -62,7 +62,7 @@ def main() -> None:
         "pyflakes": "pyflakes .",
         "mypy": "mypy . --ignore-missing-imports --strict --warn-unreachable --python-version 3.8",
         "yapf": "yapf -drp .",
-        "pylint": Shell("pylint **/*.py -E -j4"),
+        "pylint": Shell("pylint **/*.py -E -j4 --py-version=3.8"),
         #"pytype": "pytype .",
         }
 
