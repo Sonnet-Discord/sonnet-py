2efcce494aab09b55f5d6996194a3ee2e0f72dd3
ultrabear <bearodark@gmail.com>
2023-01-29 18:32:02 -0800

    fmt warning

--- libs/lib_db_obfuscator.py
+++ libs/lib_db_obfuscator.py
@@ -1,7 +1,7 @@
 # An Obfuscation layer that handles grabbing sonnet style databases
 # Ultrabear 2020
 
-# Explicitly export 
+# Explicitly export
 __all__ = ["db_hlapi", "DATABASE_FATAL_CONNECTION_LOSS"]
 
 # We now allow connection loss to be handled more gracefully
